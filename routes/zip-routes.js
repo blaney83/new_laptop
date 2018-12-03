0704b5cbd6585f263489d01d350433eb1befa1cc
Blaney83 <professionallaney@gmail.com>
2018-12-02 17:30:34 -0700

    Fixing mLab db.

--- routes/zip-routes.js
+++ routes/zip-routes.js
@@ -16,7 +16,7 @@ module.exports = function(app){
             let hbsobj = {
                 products: []
             }
-            for (let i = 1; i < 40; i++) {
+            for (let i = 1; i < 2; i++) {
                 console.log("loop working " + i)
                 // axios.interceptors.request.use(function (config) {
                 //     console.log(config)
